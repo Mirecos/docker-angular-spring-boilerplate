549b5a88a634e70ba3d499ad0ac798ce710e5187
Michel <michelrecchia1@gmail.com>
2025-09-01 21:01:27 +0200

    Fix angular project with tailwind

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { Welcome } from './pages/welcome/welcome';
-import { Home } from './pages/home/home';
 
-export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', component: Welcome },
-  { path: 'home', component: Home }
-];
+export const routes: Routes = [];
